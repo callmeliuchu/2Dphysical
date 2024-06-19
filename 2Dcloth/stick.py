506ee4b347fbf9377de3d585a520b98a73bd8e4b
callmeliuchu <1395551844@qq.com>
2024-06-19 19:58:52 +0800

    update

--- 2Dcloth/stick.py
+++ 2Dcloth/stick.py
@@ -13,19 +13,8 @@ class Stick:
     def keep_length(self):
         if not self.is_active:
             return
-        if not self.c1.is_pin and not self.c2.is_pin:
-            current = Vec2.length(self.c1.pos,self.c2.pos)
-            ratio = (current - self.length) / current * 0.5
-            diff = self.c1.pos - self.c2.pos
-            self.c1.pos -= diff * ratio
-            self.c2.pos += diff * ratio
-        if self.c1.is_pin and not self.c2.is_pin:
-            current = Vec2.length(self.c1.pos,self.c2.pos)
-            ratio = (current - self.length) / current
-            diff = self.c1.pos - self.c2.pos
-            self.c2.pos += diff * ratio
-        if not self.c1.is_pin and self.c2.is_pin:
-            current = Vec2.length(self.c1.pos,self.c2.pos)
-            ratio = (current - self.length) / current
-            diff = self.c1.pos - self.c2.pos
-            self.c1.pos -= diff * ratio
+        current = Vec2.length(self.c1.pos,self.c2.pos)
+        ratio = (current - self.length) / current * 0.5
+        diff = self.c1.pos - self.c2.pos
+        self.c1.pos -= diff * ratio
+        self.c2.pos += diff * ratio
